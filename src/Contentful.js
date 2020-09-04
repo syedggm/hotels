d5a4f263b3303776cd63d7cd1f44f550e526b352
syedggm <66167620+syedggm@users.noreply.github.com>
2020-09-04 22:01:18 +0530

    Update Contentful.js

--- src/Contentful.js
+++ src/Contentful.js
@@ -5,9 +5,4 @@ export default createClient({
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
 
-// const contentful = require('contentful')
 
-// const client = contentful.createClient({
-//   space: 'inpwempzhan5',
-//   accessToken: 'Zslq1ieMTfM9315p3NoXndGeZ3U3MG6L_hj__l8uK3c'
-// })
